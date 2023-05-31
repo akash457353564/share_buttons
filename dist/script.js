6247d0ff430d0e28a4112bcb40132881d9a8d288
akash457353564 <102581699+akash457353564@users.noreply.github.com>
2023-05-31 15:29:50 +0530

    full code

--- dist/script.js
+++ dist/script.js
@@ -574,6 +574,33 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"6rimH":[function(require,module,exports) {
+const wa_share_btn = document.querySelector(".wa_share");
+const fb_share_btn = document.querySelector(".fb_share");
+const tw_share_btn = document.querySelector(".tw_share");
+const whatsapp_share = function() {
+    const current_url = window.location.href;
+    const wa_url = `https://wa.me/?text=${encodeURIComponent(current_url)}`;
+    window.open(wa_url, "_blank");
+};
+const facebook_share = function() {
+    const current_url = window.location.href;
+    const fb_url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(current_url)}`;
+    window.open(fb_url, "_blank");
+};
+const twitter_share = function() {
+    const current_url = window.location.href;
+    const tw_url = `https://twitter.com/share?url=${encodeURIComponent(current_url)}`;
+    window.open(tw_url, "_blank");
+};
+wa_share_btn.addEventListener("click", ()=>{
+    whatsapp_share();
+});
+fb_share_btn.addEventListener("click", ()=>{
+    facebook_share();
+});
+tw_share_btn.addEventListener("click", ()=>{
+    twitter_share();
+});
 
 },{}]},["gAoaA","6rimH"], "6rimH", "parcelRequire3bab")
 
